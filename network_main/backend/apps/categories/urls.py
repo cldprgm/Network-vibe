78b6c3bd8e9a934d4f3160bc53660b73e60ec47f
cldprgm <ttgg60024@gmail.com>
2025-07-11 18:10:48 +0300

    refactored views and endpoints to support paginated communities retrieval by subcategory

--- network_main/backend/apps/categories/urls.py
+++ network_main/backend/apps/categories/urls.py
@@ -4,11 +4,13 @@ from django.conf.urls.static import static
 
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet
+from .views import CategoryViewSet, CategoryCommunityListView
 
 router = DefaultRouter()
 router.register(r'', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('subcategories/<int:subcategory_id>/communities/',
+         CategoryCommunityListView.as_view(), name='category-communities'),
 ]
--- network_main/backend/apps/categories/views.py
+++ network_main/backend/apps/categories/views.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import generics
 
-from django.db.models import Prefetch, Count, Q, OuterRef, Exists, Value
+from django.db.models import Count, Exists, Value, OuterRef
 from django.db.models.fields import BooleanField
 
 from apps.communities.models import Community
-from apps.users.models import CustomUser
+from apps.communities.serializers import CommunitySerializer
+from apps.communities.views import CommunityPagination
 from apps.memberships.models import Membership
 
 from .models import Category
@@ -19,11 +21,22 @@ class CategoryViewSet(viewsets.ModelViewSet):
     lookup_field = 'id'
 
     def get_queryset(self):
+        queryset = Category.objects.filter(
+            parent__isnull=True).prefetch_related('children')
+
+        return queryset
+
+
+class CategoryCommunityListView(generics.ListAPIView):
+    serializer_class = CommunitySerializer
+    pagination_class = CommunityPagination
+
+    def get_queryset(self):
+        subcategory_id = self.kwargs['subcategory_id']
         user = self.request.user
 
-        base_communities_qs = Community.objects.select_related('creator').annotate(
-            members_count=Count('members')
-        )
+        base_queryset = Community.objects.filter(categories=subcategory_id).select_related(
+            'creator').annotate(members_count=Count('members'))
 
         # add (members__is_approved=True) later
         if user.is_authenticated:
@@ -31,23 +44,11 @@ class CategoryViewSet(viewsets.ModelViewSet):
                 user=user,
                 community=OuterRef('pk')
             )
-            base_communities_qs = base_communities_qs.annotate(
+            base_queryset = base_queryset.annotate(
                 is_member=Exists(membership_qs))
         else:
-            base_communities_qs = base_communities_qs.annotate(
+            base_queryset = base_queryset.annotate(
                 is_member=Value(False, output_field=BooleanField())
             )
 
-        queryset = Category.objects.filter(parent__isnull=True).prefetch_related(
-            Prefetch(
-                'children',
-                queryset=Category.objects.prefetch_related(
-                    Prefetch(
-                        'communities',
-                        queryset=base_communities_qs
-                    )
-                )
-            )
-        )
-
-        return queryset
+        return base_queryset
