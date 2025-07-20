f1a3d674f3c42097af8df109d1527158e0e432d5
cldprgm <ttgg60024@gmail.com>
2025-07-20 17:27:00 +0300

    updated tests for community and membership views; assign Member role on join

--- network_main/backend/apps/communities/community_permissions.py
+++ network_main/backend/apps/communities/community_permissions.py
@@ -1,3 +1,5 @@
+from rest_framework.permissions import BasePermission
+
 from apps.memberships.models import Membership
 
 PERMISSONS_MAP = {
@@ -27,3 +29,37 @@ PERMISSONS_MAP = {
         'delete_post',
     ],
 }
+
+
+class IsCommunityCreator(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        for m in getattr(obj, 'current_user_memberships', []):
+            if m.role == Membership.Role.CREATOR:
+                return True
+        return False
+
+
+class HasCommunityPermission(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        permission = getattr(view, 'requiered_permission', None)
+        if not permission or not request.user.is_authenticated:
+            return False
+        roles = obj.members.filter(
+            user=request.user).values_list('role', flat=True)
+        user_permissions = {
+            permission
+            for role in roles
+            for permission in PERMISSONS_MAP.get(role, [])
+        }
+        print(user_permissions)
+        return permission in user_permissions
+
+
+class CannotLeaveIfCreator(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if obj.role == Membership.Role.CREATOR:
+            return False
+        return True
