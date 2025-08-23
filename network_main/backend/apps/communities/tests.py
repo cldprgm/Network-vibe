825d7fd732c65894b804225bfca513daa7792055
cldprgm <ttgg60024@gmail.com>
2025-08-23 23:10:43 +0300

    test: extend TestCommunityViewSet tests

--- network_main/backend/apps/communities/tests.py
+++ network_main/backend/apps/communities/tests.py
@@ -301,6 +301,18 @@ class TestCommunityViewSet():
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
         assert Community.objects.filter(pk=community.pk).exists()
 
+    def test_create_community_duplicate_slug(self, authenticated_client, community, category):
+        url = reverse('community-list')
+        data = {
+            'name': 'another_name',
+            'slug': community.slug,
+            'description': 'desc',
+            'categories': category.pk
+        }
+        response = authenticated_client.post(url, data)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'slug' in response.data
+
 
 @pytest.mark.django_db
 class TestCommunityPostsListView():
