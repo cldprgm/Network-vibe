d18d758ad682df009b1b42404f09bb86c3fa68ab
cldprgm <dlyaforza@gmail.com>
2025-10-08 15:22:46 +0000

    feat: update pre-prod

--- network_main/frontend/eslint.config.mjs
+++ network_main/frontend/eslint.config.mjs
@@ -10,17 +10,31 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  // for the test
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
+    // for the test build(don't use it)
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+
+      "@next/next/no-img-element": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "@typescript-eslint/no-unused-expressions": "off", 
+      "react/display-name": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "jsx-a11y/alt-text": "off",
+
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
     },
-    languageOptions: {
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-    },
+    // languageOptions: {
+    //   parserOptions: {
+    //     project: "./tsconfig.json",
+    //   },
+    // },
   }),
 ];
 
--- network_main/frontend/next.config.ts
+++ network_main/frontend/next.config.ts
@@ -1,13 +1,21 @@
 import type { NextConfig } from "next";
 
+const baseUrl = process.env.NEXT_PUBLIC_API_ASSETS_URL;
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: [`http://${baseUrl}:80`, `http://${baseUrl}:3000`, "http://apibackend:8001", "http://localhost"],
   images: {
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'apibackend',
         port: '8001',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'http',
+        hostname: `${baseUrl}`,
         pathname: '/media/**',
       },
       {
