38cb1acfecdd861afb8c92a2b544e965e6c66c37
cldprgm <ttgg60024@gmail.com>
2025-10-19 21:57:10 +0300

    feat(backend): add user endpoint

--- network_main/backend/apps/users/urls.py
+++ network_main/backend/apps/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (
     CustomUserView,
+    CustomUserInfoView,
     UserRegistrationView,
     LogoutView,
     LoginView,
@@ -12,11 +13,12 @@ from .views import (
 
 
 urlpatterns = [
-    path('user-info/', CustomUserView.as_view(), name='user-info'),
+    path('user-info/', CustomUserInfoView.as_view(), name='user-info'),
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('refresh/', CookieTokenRefreshView.as_view(), name='refresh'),
     path('verify/', VerifyCodeView.as_view(), name='verify_code'),
     path('resend/', ResendVerificationView.as_view(), name='resend_code'),
+    path('<slug:slug>/', CustomUserView.as_view(), name='user'),
 ]
