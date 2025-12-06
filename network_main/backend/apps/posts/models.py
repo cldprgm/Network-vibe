bcc1b936a9f47cf86855fab542720684181ea7ef
cldprgm <ttgg60024@gmail.com>
2025-12-06 21:15:45 +0300

    feat(backend): change the way post media files are uploaded. fix post tests
    aspect ratio is now calculated only if file has not been previously uploaded, so as not to download it from s3

--- network_main/backend/apps/posts/models.py
+++ network_main/backend/apps/posts/models.py
@@ -241,22 +241,22 @@ class Media(models.Model):
         return 'unknown'
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if self.get_media_type() == 'image':
+        if self._state.adding and self.file:
             self.aspect_ratio = self._compute_aspect_ratio()
-            super().save(update_fields=['aspect_ratio'])
+        super().save(*args, **kwargs)
 
     def _compute_aspect_ratio(self):
         try:
-            with self.file.open('rb') as file:
-                parser = ImageFile.Parser()
-                header = file.read(8192)
-                parser.feed(header)
-                img = parser.close()
-                width, height = img.size
-                return f'{width}/{height}'
-        except (DecompressionBombError, UnidentifiedImageError, Exception):
+            self.file.open()
+            image = Image.open(self.file)
+            width, height = image.size
+            ratio = f"{width}/{height}"
+
+            return ratio
+        except Exception:
             return "16/9"
+        finally:
+            self.file.seek(0)
 
     @property
     def get_aspect_ratio(self):
